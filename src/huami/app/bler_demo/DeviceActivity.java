cecbde269ff835cb05caca98ca8033da27977005
Gu Yan <yan.goo@gmail.com>
2014-11-28 13:38:30 +0800

    UnlockProfile (deprecated)

--- src/huami/app/bler_demo/DeviceActivity.java
+++ src/huami/app/bler_demo/DeviceActivity.java
@@ -1,8 +1,7 @@
 package huami.app.bler_demo;
 
 import huami.dev.bler.core.IConnectionStateChangeCallback;
-import huami.dev.bler.gatt.profile.ImmediateAlertProfile;
-import huami.dev.bler.gatt.service.IImmediateAlertService;
+import huami.dev.bler.gatt.profile.UnlockProfile;
 import huami.dev.util.Logdog;
 import android.app.Activity;
 import android.bluetooth.BluetoothDevice;
@@ -12,8 +11,7 @@ import android.widget.Toast;
 
 public final class DeviceActivity extends Activity {
 
-	private ImmediateAlertProfile m_Peripheral = null;
-	private static final int APPID = 1000; // 由小米手环开发者平台分配
+	private UnlockProfile m_Peripheral = null;
 	private static final byte[] KEY = new byte[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16 }; // 应用自行生成，需妥善保管
 
 	@Override
@@ -22,7 +20,7 @@ public final class DeviceActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_device);
 		final BluetoothDevice device = getIntent().getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-		m_Peripheral = new ImmediateAlertProfile(this, device, new IConnectionStateChangeCallback() {
+		m_Peripheral = new UnlockProfile(this, device, new IConnectionStateChangeCallback() {
 
 			@Override
 			public void onConnected(BluetoothDevice device) {
@@ -47,7 +45,7 @@ public final class DeviceActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getAuthService().authroize(APPID, KEY);
+				final boolean ret = m_Peripheral.getUnlockService().authorize(KEY);
 				toast(ret ? "授权成功" : "授权失败，请检查是否已连接，并确认在亮灯后敲击手环");
 			}
 
@@ -56,44 +54,17 @@ public final class DeviceActivity extends Activity {
 
 			@Override
 			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getAuthService().authenticate(APPID, KEY);
+				final boolean ret = m_Peripheral.getUnlockService().authenticate(KEY);
 				toast(ret ? "认证成功" : "认证失败，可能原因：未连接或与appid对应的密钥错误");
 			}
 
 		});
-		findViewById(R.id.btn_confirm).setOnClickListener(new View.OnClickListener() {
+		findViewById(R.id.btn_read_rssi).setOnClickListener(new View.OnClickListener() {
 
 			@Override
 			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getAuthService().confirm(APPID);
-				toast(ret ? "已确认" : "未确认，请检查是否已连接，并确认在亮灯后敲击手环");
-			}
-
-		});
-		findViewById(R.id.btn_alert1).setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getImmediateAlertService().alert(IImmediateAlertService.MILD_ALERT);
-				toast(ret ? "命令成功" : "命令失败，可能原因：未连接");
-			}
-
-		});
-		findViewById(R.id.btn_alert2).setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getImmediateAlertService().alert(IImmediateAlertService.HIGH_ALERT);
-				toast(ret ? "命令成功" : "命令失败，可能原因：未连接");
-			}
-
-		});
-		findViewById(R.id.btn_cancel_alert).setOnClickListener(new View.OnClickListener() {
-
-			@Override
-			public void onClick(View v) {
-				final boolean ret = m_Peripheral.getImmediateAlertService().alert(IImmediateAlertService.NO_ALERT);
-				toast(ret ? "命令成功" : "命令失败，可能原因：未连接");
+				final int rssi = m_Peripheral.readRemoteRSSI();
+				toast("RSSI = " + rssi);
 			}
 
 		});
